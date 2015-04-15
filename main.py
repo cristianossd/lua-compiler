05404633c915233b2c73f9cdf44e2d356c9ad674
Cristiano Santos <cristianossd@gmail.com>
2015-04-15 19:43:58 -0300

    Adding the rules for more tokens. Definied the simple regular expression to match strings

--- main.py
+++ main.py
@@ -27,22 +27,45 @@ reserved = {
 
 # List of token names
 tokens = [
-  'ID', 'NUMBER',
-  'CHAR', 'STRING',
-  'PLUS', 'MINUS',
-  'TIMES', 'DIVIDE',
-  'PERCENT', 'CIRCUMFLEX',
-  'SHARP', 'EQUAL',
-  'NOTEQUAL', 'LESSEQUAL',
-  'GREATEREQUAL', 'LESS',
-  'GREATER', 'ASSIGN',
-  'LPAREN', 'RPAREN',
-  'LCURLY', 'RCURLY',
-  'LSQUARE', 'RSQUARE'
-  'SEMICOLON', 'COLON',
-  'COMMA', 'DOT',
-  'TWODOTS', 'THREEDOTS'
+  'ID'        , 'NUMBER'      , 'STRING',
+  'PLUS'      , 'MINUS'       , 'TIMES',
+  'DIVIDE'    , 'PERCENT'     , 'CIRCUMFLEX',
+  'SHARP'     , 'EQUAL'       , 'NOTEQUAL',
+  'LESSEQUAL' , 'GREATEREQUAL', 'LESS',
+  'GREATER'   , 'ASSIGN'      , 'LPAREN',
+  'RPAREN'    , 'LCURLY'      , 'RCURLY',
+  'LSQUARE'   , 'RSQUARE'     , 'SEMICOLON',
+  'COLON'     , 'COMMA'       , 'DOT',
+  'TWODOTS'   , 'THREEDOTS'
 ] + list(reserved.values())
+
+t_STRING        = r'(\'.+\')|(\".+\")'
+t_PLUS          = r'\+'
+t_MINUS         = r'-'
+t_TIMES         = r'\*'
+t_DIVIDE        = r'/'
+t_PERCENT       = r'%'
+t_CIRCUMFLEX    = r'\^'
+t_SHARP         = r'#'
+t_EQUAL         = r'=='
+t_NOTEQUAL      = r'~='
+t_LESSEQUAL     = r'<='
+t_GREATEREQUAL  = r'>='
+t_LESS          = r'<'
+t_GREATER       = r'>'
+t_ASSIGN        = r'='
+t_LPAREN        = r'\('
+t_RPAREN        = r'\)'
+t_LCURLY        = r'\{'
+t_RCURLY        = r'}'
+t_LSQUARE       = r'\['
+t_RSQUARE       = r'\]'
+t_SEMICOLON     = r';'
+t_COLON         = r':'
+t_COMMA         = r','
+t_DOT           = r'\.'
+t_TWODOTS       = r'\.\.'
+t_THREEDOTS     = r'\.\.\.'
 
 # Regular expressions rules
 def t_COMMENT(t):
@@ -54,6 +77,11 @@ def t_COMMENT(t):
 def t_ID(t):
   r'[a-zA-Z_][a-zA-Z_0-9]*'
   t.type = reserved.get(t.value, 'ID')
+  return t
+
+def t_NUMBER(t):
+  r'\d+'
+  t.value = int(t.value)
   return t
 
 t_ignore = ' \t'
