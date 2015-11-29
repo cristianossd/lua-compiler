8f70090472c8c850f5cf74933c19a85e0fe2f06f
Cristiano Santos <cristianossd@gmail.com>
2015-11-29 05:20:09 -0300

    Printing parser list as an AST

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import parser
 import sys
 from sys import argv
 
+def print_as_tree(lst, level = 1):
+  print('--' * (level-1)) + str(lst[0])
+  for l in lst[1:]:
+    if type(l) != tuple:
+      print ('-- ' * level) + str(l)
+    else:
+      print_as_tree(l, level+1)
+
+
 if __name__ == '__main__':
   filename = argv[1]
   f = open(filename)
@@ -26,3 +35,4 @@ if __name__ == '__main__':
 
   result = parser.parser.parse(code, tracking = True)
   print result
+  print_as_tree(result)
--- parser.py
+++ parser.py
@@ -20,7 +20,7 @@ def p_blocklist(p):
     if len(p) == 2:
        p[0] = p[1]
     else:
-       p[0] = ('BLOCKLIST', p[1], p[2])
+       p[0] = ('blocklist', p[1], p[2])
 
 def p_blockterminator(p):
     '''blockterminator : empty
@@ -34,11 +34,11 @@ def p_command(p):
                | WHILE exp DO block END
                | IF exp THEN block elsestnt END'''
     if len(p) == 4:
-        p[0] = ('ASSIGN', p[1], p[3])
+        p[0] = ('assign', p[1], p[3])
     elif len(p) == 6:
-        p[0] = ('WHILE', p[2], p[4])
+        p[0] = ('while', p[2], p[4])
     elif len(p) == 7:
-        p[0] = ('IF', p[2], p[4], p[5])
+        p[0] = ('if', p[2], p[4], p[5])
     else:
         p[0] = p[1]
 
@@ -48,7 +48,7 @@ def p_elsestnt(p):
     if len(p) == 2:
         p[0] = p[1]
     else:
-        p[0] = ('ELSE', p[2])
+        p[0] = ('else', p[2])
 
 def p_exp(p):
     '''exp : NUMBER
@@ -61,19 +61,19 @@ def p_exp(p):
         if p[1] == '(':
             p[0] = p[2]
         else:
-            p[0] = ('BINARY OPERATION', p[1], p[2], p[3])
+            p[0] = ('binary-operation', p[1], p[2], p[3])
     elif len(p) == 3:
-        p[0] = ('UNARY OPERATION', p[1], p[2])
+        p[0] = ('unary-operation', p[1], p[2])
     else:
         p[0] = p[1]
 
 def p_functioncall(p):
     '''functioncall : ID LPAREN explist RPAREN'''
-    p[0] = ('FUNCTION CALL', p[1], p[3])
+    p[0] = ('function-call', p[1], p[3])
 
 def p_vardeclaration(p):
     '''vardeclaration : VAR ID expassign'''
-    p[0] = ('VAR', p[2], p[3])
+    p[0] = ('var', p[2], p[3])
 
 def p_expassign(p):
     '''expassign : empty
@@ -89,7 +89,7 @@ def p_explist(p):
     if len(p) == 2:
        p[0] = p[1]
     else:
-       p[0] = ('EXP LIST', p[1], p[2])
+       p[0] = ('exp-list', p[1], p[2])
 
 def p_lexp(p):
     '''lexp : empty
@@ -97,7 +97,7 @@ def p_lexp(p):
     if len(p) == 2:
         p[0] = p[1]
     else:
-        p[0] = ('EXP SEQ', p[1], p[2])
+        p[0] = ('exp-seq', p[1], p[2])
 
 def p_binop(p):
     '''binop : PLUS
@@ -112,12 +112,12 @@ def p_binop(p):
              | NOTEQUAL
              | AND
              | OR'''
-    p[0] = ('BINARY OPERATOR', p[1])
+    p[0] = ('binary-operator', p[1])
 
 def p_unaryop(p):
     '''unaryop : MINUS %prec UMINUS
                | NOT'''
-    p[0] = ('UNARY OPERATOR', p[1])
+    p[0] = ('unary-operator', p[1])
 
 def p_empty(p):
     'empty : '
