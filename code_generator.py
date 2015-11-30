dc3eb445d30c7d095faa3f7a08bcd0562d7af207
Cristiano Santos <cristianossd@gmail.com>
2015-11-30 01:11:20 -0300

    MIPS instructions for var declaration and assignment

--- code_generator.py
+++ code_generator.py
@@ -16,66 +16,94 @@ class CodeGenerator:
       else:
         self.print_tree(l, level+1)
 
+  # MIPS instructions
+  def mips_uminus_term():
+    self.mips_top_stack_on_a0()
+    self.mips_out += "lw $t1, $a0\n"
+    self.mips_out += "sub $a0, $a0, $a0\n"
+    self.mips_out += "sub $a0, $a0, $t1\n"
+    self.mips_push_a0_on_stack()
+
+  def mips_top_stack_on_a0(self):
+    self.mips_out += "lw $a0, 4($sp)\n"
+    self.mips_out += "addiu $sp, $sp 4\n"
+
+  def mips_top_stack_on_t1(self):
+    self.mips_out += "lw $t1, 4($sp)\n"
+    self.mips_out += "addiu $sp, $sp 4\n"
+
+  def mips_top_stack_on_t2(self):
+    self.mips_out += "lw $t2, 4($sp)\n"
+    self.mips_out += "addiu $sp, $sp 4\n"
+
+  def mips_get_two_tmp(self):
+    self.mips_out += "lw $t1, 0($sp)\n"
+    self.mips_out += "addiu $sp, $sp, 4\n"
+    self.mips_out += "lw $t2, 0($sp)\n"
+    self.mips_out += "addiu $sp, $sp, 4\n"
+
+  def mips_push_a0_on_stack(self):
+    self.mips_out += "sw $a0, 0($sp)\n"
+    self.mips_out += "addiu $sp, $sp, -4\n"
+
+  def mips_assign_a0(self, value):
+    self.mips_out += "li $a0, " + value + "\n"
+
+  def mips_assign_t1(self, value):
+    self.mips_out += "li $t1, " + value + "\n"
+
+  def mips_assign_t2(self, value):
+    self.mips_out += "li $t2, " + value + "\n"
+
+  def mips_store_var(self, value):
+    self.mips_out += "sw $a0, " + value + "\n"
+
   # Accessing var table
   def get_var(self, var):
+    print var
     v = self.var_table.get(var)
+    print v
     if v == None:
       print "Semantic error: Trying to use undeclared variable '%s'" % var
       sys.exit()
 
   # Making binary operation
-  def make_bin_operation(self, first, op, second):
+  def make_bin_operation(self, op):
     if op == '+':
-      self.mips_out += "li $a0, " + str(first) + "\n"
-      self.mips_out += "sw $a0, 0($sp)\n"
-      self.mips_out += "addiu $sp $sp -4\n"
-      self.mips_out "li $a0, " + str(second) "\n"
-      self.mips_out "lw $t1 4($sp)\n"
-      self.mips_out "add $a0 $t1 $a0\n"
-      self.mips_out "addiu $sp $sp 4\n"
+      self.mips_get_two_tmp()
+      self.mips_out += "add $a0, $t2, $t1\n"
+      self.mips_push_a0_on_stack()
     elif op == '-':
-      self.mips_out += "li $a0, " + str(first) + "\n"
-      self.mips_out += "sw $a0, 0($sp)\n"
-      self.mips_out += "addiu $sp $sp -4\n"
-      self.mips_out "li $a0, " + str(second) "\n"
-      self.mips_out "lw $t1 4($sp)\n"
-      self.mips_out "sub $a0 $t1 $a0\n"
-      self.mips_out "addiu $sp $sp 4\n"
+      self.mips_get_two_tmp()
+      self.mips_out += "sub $a0, $t2, $t1\n"
+      self.mips_push_a0_on_stack()
     elif op == '*':
-      self.mips_out += "li $a0, " + str(first) + "\n"
-      self.mips_out += "sw $a0, 0($sp)\n"
-      self.mips_out += "addiu $sp $sp -4\n"
-      self.mips_out "li $a0, " + str(second) "\n"
-      self.mips_out "lw $t1 4($sp)\n"
-      self.mips_out "mul $a0 $t1 $a0\n"
-      self.mips_out "addiu $sp $sp 4\n"
+      self.mips_get_two_tmp()
+      self.mips_out += "mul $a0, $t2, $t1\n"
+      self.mips_push_a0_on_stack()
     elif op == '/':
-      self.mips_out += "li $a0, " + str(first) + "\n"
-      self.mips_out += "sw $a0, 0($sp)\n"
-      self.mips_out += "addiu $sp $sp -4\n"
-      self.mips_out "li $a0, " + str(second) "\n"
-      self.mips_out "lw $t1 4($sp)\n"
-      self.mips_out "div $a0 $t1 $a0\n"
-      self.mips_out "addiu $sp $sp 4\n"
+      self.mips_get_two_tmp()
+      self.mips_out += "div $a0, $t2, $t1\n"
+      self.mips_push_a0_on_stack()
     elif op == '==':
-      return first == second
+      return
     elif op == '~=':
-      return first != second
+      return
     elif op == '<=':
-      return first <= second
+      return
     elif op == '>=':
-      return first >= second
+      return
     elif op == '<':
-      return first < second
+      return
     elif op == '>':
-      return first > second
+      return
 
   # Making unary operation
-  def make_unary_operation(self, term, op):
+  def make_unary_operation(self, op):
     if op == '-':
-      return -term
+      self.mips_top_stack_on_t1()
     elif op == 'NOT':
-      return not term
+      return
 
   # AST blocks
   # Var declaration
@@ -95,29 +123,33 @@ class CodeGenerator:
     if self.defining_var is True:
       return
 
-    if node[1] is tuple:
+    if type(node[1]) is tuple:
       if node[1][0] == 'binary-operation':
-        first = self.binary_operation_statement(node[1])
+        self.binary_operation_statement(node[1])
       elif node[1][0] == 'unary-operation':
-        first = self.unary_operation_statement(node[1])
+        self.unary_operation_statement(node[1])
       else:
-        first = self.function_call_statement(node[1])
+        self.function_call_statement(node[1])
     else:
-      first = node[1] if type(node[1]) is int else self.get_var(node[1])
+      value = node[1] if type(node[1]) is int else self.get_var(node[1])
+      self.mips_assign_a0(str(value))
+      self.mips_push_a0_on_stack()
 
-    if node[3] is tuple:
+    if type(node[3]) is tuple:
       if node[3][0] == 'binary-operation':
-        second = self.binary_operation_statement(node[3])
+        self.binary_operation_statement(node[3])
       elif node[3][0] == 'unary-operation':
-        second = self.unary_operation_statement(node[3])
+        self.unary_operation_statement(node[3])
       else:
-        second = self.function_call_statement(node[3])
+        self.function_call_statement(node[3])
     else:
-      second = node[3] if type(node[3]) is int else self.get_var(node[3])
+      value = node[3] if type(node[3]) is int else self.get_var(node[3])
+      self.mips_assign_a0(str(value))
+      self.mips_push_a0_on_stack()
 
     op = node[2][1]
-
-    return self.make_bin_operation(first, op, second)
+    self.make_bin_operation(op)
+    return
 
   # Unary Operation
   def unary_operation_statement(self, node):
@@ -125,17 +157,19 @@ class CodeGenerator:
       return
 
     op = node[1]
-    if node[2] is tuple:
+    if type(node[2]) is tuple:
       if node[2][0] == 'binary-operation':
-        term = self.binary_operation_statement(node[2])
+        self.binary_operation_statement(node[2])
       elif node[2][0] == 'unary-operation':
-        term = self.unary_operation_statement(node[2])
+        self.unary_operation_statement(node[2])
       else:
-        term = self.function_call_statement(node[2])
+        self.function_call_statement(node[2])
     else:
       term = node[2] if type(node[2]) is int else self.get_var(node[2])
+      self.mips_assign_a0(str(term))
+      self.mips_push_a0_on_stack()
 
-    self.make_unary_operation(term, op)
+    self.make_unary_operation(op)
     return
 
   # Assign
@@ -161,26 +195,22 @@ class CodeGenerator:
 
       if type(var[1]) is tuple:
         if var[1][0] == 'binary-operation':
-          self.binary_operation_statement(var[1])
-          self.mips_out += "sw $t1, 4($sp)\n"
-          self.mips_out += "addiu $sp, $sp 4\n"
-          self.mips_out += "sw $a0, $t1\n"
+          self.binary_operation_statement(var[1]) # must push the recursive return
+          self.mips_top_stack_on_a0()
           value = 'unassigned'
         elif var[1][0] == 'unary-operation':
-          self.unary_operation_statement(var[1])
-          self.mips_out += "sw $t1, 4($sp)\n"
-          self.mips_out += "addiu $sp, $sp 4\n"
-          self.mips_out += "sw $a0, $t1\n"
+          self.unary_operation_statement(var[1]) # must push the recursive return
+          self.mips_top_stack_on_a0()
           value = 'unassigned'
         else:
           print "Semantic error: can not assign its value for '%s' variable" % var[0]
           sys.exit()
       else:
         value = var[1]
-        self.mips_out += "li $a0, " + str(value) + "\n"
+        self.mips_assign_a0(str(value))
 
       self.var_table[index] = value
-      self.mips_out += "sw $a0, " + str(index) + "\n\n"
+      self.mips_store_var(str(index))
 
   def var_declaration_exception(self):
     if self.defining_var:
